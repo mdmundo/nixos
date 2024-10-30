ffbce45291c88d417717f25fb2767a50c9fbd25e
Edmundo Paulino <manyymoore@gmail.com>
2024-10-30 06:47:35 -0300

    xfce

--- configuration.nix
+++ configuration.nix
@@ -51,8 +51,11 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.defaultSession = "xfce";
+  services.xserver.desktopManager = {
+    xterm.enable = false;
+    xfce.enable = true;
+  };
 
   # Configure keymap in X11
   services.xserver.xkb.layout = "br";

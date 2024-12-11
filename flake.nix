67358756df3ca186c9843cfe36f0d208ce5f266a
Edmundo <manyymoore@gmail.com>
2024-12-11 16:06:46 -0300

    first atempt

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    { nixpkgs, home-manager, ... }@inputs:
+    {
+      nixosConfigurations = {
+        nitro = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            nitro/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.edmundo = import nitro/home.nix;
+            }
+          ];
+        };
+      };
+    };
+}

c4e5d0ed2ccc8ff79976a43933f598ae2ba15387
Edmundo <manyymoore@gmail.com>
2024-12-11 18:25:31 -0300

    rm plasma

--- flake.nix
+++ flake.nix
@@ -7,20 +7,11 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    plasma-manager = {
-      url = "github:nix-community/plasma-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
-    };
+
   };
 
   outputs =
-    {
-      nixpkgs,
-      home-manager,
-      plasma-manager,
-      ...
-    }@inputs:
+    { nixpkgs, home-manager, ... }@inputs:
     {
       nixosConfigurations = {
         nitro = nixpkgs.lib.nixosSystem {
@@ -33,7 +24,6 @@
                 useGlobalPkgs = true;
                 useUserPackages = true;
                 users.edmundo = import nitro/home/edmundo.nix;
-                sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
               };
             }
           ];

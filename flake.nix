a85b8fd5b359dbe01a40d79aed63daca155747cc
Edmundo <manyymoore@gmail.com>
2024-12-11 18:18:49 -0300

    plasma shortcuts

--- flake.nix
+++ flake.nix
@@ -3,12 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }@inputs:
+    {
+      nixpkgs,
+      home-manager,
+      plasma-manager,
+      ...
+    }@inputs:
     {
       nixosConfigurations = {
         nitro = nixpkgs.lib.nixosSystem {
@@ -17,9 +29,12 @@
             nitro/configuration.nix
             home-manager.nixosModules.home-manager
             {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.edmundo = import nitro/home/edmundo.nix;
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.edmundo = import nitro/home/edmundo.nix;
+                sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+              };
             }
           ];
         };
--- nitro/home/edmundo.nix
+++ nitro/home/edmundo.nix
@@ -22,7 +22,27 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  programs = {
-    mise.enable = true;
+  programs.plasma = {
+    enable = true;
+    shortcuts = {
+      "kmix"."decrease_volume" = "Meta+Down,Volume Down,Decrease Volume";
+      "kmix"."increase_volume" = "Meta+Up,Volume Up,Increase Volume";
+      "ksmserver"."Log Out" = "Meta+Del,Ctrl+Alt+Del,Log Out";
+      "kwin"."Window Close" = "Meta+Q,Alt+F4,Close Window";
+      "kwin"."Window Maximize" = "Meta+PgUp";
+      "kwin"."Window Minimize" = "Meta+PgDown";
+      "mediacontrol"."nextmedia" = "Meta+L,Media Next,Media playback next";
+      "mediacontrol"."playpausemedia" = "Meta+K,Media Play,Play/Pause media playback";
+      "mediacontrol"."previousmedia" = "Meta+J,Media Previous,Media playback previous";
+      "org_kde_powerdevil"."Decrease Screen Brightness" = "Meta+Left,Monitor Brightness Down,Decrease Screen Brightness";
+      "org_kde_powerdevil"."Increase Screen Brightness" = "Meta+Right,Monitor Brightness Up,Increase Screen Brightness";
+      "plasmashell"."show-on-mouse-pos" = "Meta+V";
+    };
+    configFile = {
+      "klipperrc"."General"."IgnoreImages" = false;
+      "klipperrc"."General"."MaxClipItems" = 2048;
+      "kscreenlockerrc"."Daemon"."Autolock" = false;
+      "kscreenlockerrc"."Daemon"."LockGrace" = 0;
+    };
   };
 }

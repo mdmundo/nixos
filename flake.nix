4bd44fcc625b88ac522a34fce3deb3914ff1d969
Edmundo <manyymoore@gmail.com>
2024-12-11 16:27:13 -0300

    mv

--- flake.nix
+++ flake.nix
@@ -14,12 +14,12 @@
         nixos = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            nixos/configuration.nix
+            ./configuration.nix
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
-              home-manager.users.edmundo = import nixos/home.nix;
+              home-manager.users.edmundo = import ./home.nix;
             }
           ];
         };

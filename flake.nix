ea345bb88236cc6695395554af395c91093c250c
Edmundo <manyymoore@gmail.com>
2024-12-12 07:37:11 -0300

    release

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/7109b680d161993918b0a126f38bc39763e5a709";
-    updates.url = "github:nixos/nixpkgs/a0f3e10d94359665dba45b71b4227b0aeb851f8e";
+    updates.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";

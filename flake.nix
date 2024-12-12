34ac9ed7b4948e06a943b8cdffee8bbf3db4148c
Edmundo <manyymoore@gmail.com>
2024-12-12 07:04:58 -0300

    revision

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/7109b680d161993918b0a126f38bc39763e5a709";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,7 +11,7 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }@inputs:
+    inputs@{ nixpkgs, home-manager, ... }:
     {
       nixosConfigurations = {
         nitro = nixpkgs.lib.nixosSystem {

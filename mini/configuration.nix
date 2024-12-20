63e430831119ea397cfdfd1ce9d1516777699eb3
Edmundo <manyymoore@gmail.com>
2024-12-20 20:50:56 -0300

    reset

--- mini/configuration.nix
+++ mini/configuration.nix
@@ -151,8 +151,6 @@
 
   hardware.bluetooth.enable = true;
 
-  services.samba.enable = true;
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

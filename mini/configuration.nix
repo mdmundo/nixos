673f9821a4d46fd1d1a3a90dcfdbe80e91786fab
Edmundo <manyymoore@gmail.com>
2024-12-20 20:12:03 -0300

    samba

--- mini/configuration.nix
+++ mini/configuration.nix
@@ -150,6 +150,8 @@
 
   hardware.bluetooth.enable = true;
 
+  services.samba.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

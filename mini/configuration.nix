c66d5419667d0e9763afe5bd047d26706a62fe63
Edmundo <manyymoore@gmail.com>
2024-12-21 10:20:10 -0300

    sftp

--- mini/configuration.nix
+++ mini/configuration.nix
@@ -153,6 +153,8 @@
     enableAMD = true;
   };
 
+  services.openssh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

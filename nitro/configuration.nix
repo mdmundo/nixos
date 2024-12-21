05e882ea7de01878ae641ba8067d93505f66d4fe
Edmundo <manyymoore@gmail.com>
2024-12-21 10:20:49 -0300

    sftp

--- nitro/configuration.nix
+++ nitro/configuration.nix
@@ -183,6 +183,8 @@
     enableAMD = true;
   };
 
+  services.openssh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

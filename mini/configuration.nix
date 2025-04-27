77f49043b7aa59316b561bf0e0ae1f8d978bd00e
Edmundo <60408300+mdmundo@users.noreply.github.com>
2025-04-27 09:54:45 -0300

    enable vbox

--- mini/configuration.nix
+++ mini/configuration.nix
@@ -209,6 +209,9 @@
     };
   };
 
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "edmundo" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

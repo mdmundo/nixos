00968047d5539aa3040b63ce80f310b3fc681033
Edmundo <60408300+mdmundo@users.noreply.github.com>
2026-01-09 09:39:06 -0300

    install nh

--- mini/configuration.nix
+++ mini/configuration.nix
@@ -200,6 +200,8 @@
     };
   };
 
+  programs.nh.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
